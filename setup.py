d25d2d97ce94867b6bc14f9e225859378ee0e0ea
Ricardo-Pascual <textex204@gmail.com>
2017-01-06 17:28:43 -0600

    create project

---
+++ setup.py
@@ -0,0 +1,16 @@
+from rsr_calculator.version import version
+
+from distutils.core import setup
+
+setup(
+    name='test_pip',
+    version=version,
+    description='description',
+    long_description='long description',
+    url='https://github.com/Ryszard-Ps/test-pip.git',
+    author='Ryszard-Ps',
+    author_email='hello@rjbits.com',
+    license='MIT',
+    package_dir={'': 'test_pip'},
+    packages=['test_pip']
+)
